992fc27b4d680cb115fdbe5acd0816eb616f2d5f
Andrew Trent <andrewtrentdesign@gmail.com>
2018-12-03 23:15:50 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new_artist = self.new(name)
+    new_artist.save
+    new_artist
+  end
+
+  def add_song(song)
+    if song.artist != self
+      song.artist = self
+    end
+    #if self.songs.include?(song) == false
+  #    self.songs << song
+  #  end
+  end
+
+
+
+end
