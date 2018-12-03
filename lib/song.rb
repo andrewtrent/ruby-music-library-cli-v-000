2dc85f3e4944e13a43de7b0729440edc1ac3f0b4
Andrew Trent <andrewtrentdesign@gmail.com>
2018-12-03 23:13:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist = artist if artist != nil
+    @genre = genre if genre != nil
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new_song = self.new(name)
+    new_song.save
+    new_song
+  end
+
+  def genre=(genre)
+    genre.songs << self
+  end
+
+  def artist=(artist)
+    artist.add_song(self)
+  end
+end
